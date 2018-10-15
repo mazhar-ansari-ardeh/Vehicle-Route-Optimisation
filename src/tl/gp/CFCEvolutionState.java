526ff0fbec1246bacd086bfdd1011dd015da8bbc
mzhr <mzhr@10.140.146.156>
2018-10-15 16:30:59 +1300

    Marked as deprecated.

--- src/tl/gp/CFCEvolutionState.java
+++ src/tl/gp/CFCEvolutionState.java
@@ -2,6 +2,7 @@ package tl.gp;
 
 import ec.simple.SimpleEvolutionState;
 
+@Deprecated
 public class CFCEvolutionState extends SimpleEvolutionState
 {
 	private static final long serialVersionUID = 1L;
