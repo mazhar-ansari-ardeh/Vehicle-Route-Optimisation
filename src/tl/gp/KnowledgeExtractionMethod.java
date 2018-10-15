eb83454617ee4e37e36f3f984c2baed9c5570e9c
mzhr <mzhr@10.140.146.156>
2018-10-15 13:20:11 +1300

    added the parse method. added fields ExactCodeFragment, AllSubtrees

--- src/tl/gp/KnowledgeExtractionMethod.java
+++ src/tl/gp/KnowledgeExtractionMethod.java
@@ -3,12 +3,41 @@ package tl.gp;
 public enum KnowledgeExtractionMethod
 {
 	/**
+	 * The knowledge source contains exact code fragments so that loaded code fragments do not need 
+	 * to be processed after loading. 
+	 */
+	ExactCodeFragment, 
+	/**
 	 * All possible subtrees of a tree will be extracted
 	 */
-	All,
+	AllSubtrees,
 
 	/**
 	 * Only subtrees of root will be sliced out.
 	 */
-	RootSubtree;
+	RootSubtree,
+	
+	/**
+	 * Only consider the root of individual. As a matter of fact, this method considers the whole
+	 * individual as a code fragment.
+	 */
+	Root;
+	
+	public static KnowledgeExtractionMethod parse(String method)
+	{
+		if(method.equals("exactcodefragment"))
+			return ExactCodeFragment;
+		if(method.equals("all"))
+			return KnowledgeExtractionMethod.AllSubtrees;
+		else if(method.equals("rootsubtree"))
+			return KnowledgeExtractionMethod.RootSubtree;
+		else if(method.equals("root"))
+			return KnowledgeExtractionMethod.Root;
+		else
+		{
+			throw new IllegalArgumentException("Invalid value for parameter knowledge-extraction: " 
+												+ method
+												+ "Acceptable values are: 'all' and 'root'.");
+		}
+	}
 }
