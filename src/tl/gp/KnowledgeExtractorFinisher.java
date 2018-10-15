e40c9f97d5a99732b51100450a24b437d16579dc
mzhr <mzhr@10.140.146.156>
2018-10-15 16:54:20 +1300

    new additions.

---
+++ src/tl/gp/KnowledgeExtractorFinisher.java
@@ -0,0 +1,126 @@
+package tl.gp;
+
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.ObjectInputStream;
+import java.io.ObjectOutputStream;
+import java.util.ArrayList;
+
+import ec.*;
+import ec.gp.*;
+import ec.simple.SimpleFinisher;
+import ec.simple.SimpleProblemForm;
+import ec.util.Parameter;
+import tl.knowledge.codefragment.fitted.DoubleFittedCodeFragment;
+
+public class KnowledgeExtractorFinisher extends SimpleFinisher 
+{
+	private static final long serialVersionUID = 1L;
+
+	private KnowledgeExtractionMethod extractionMethod;
+
+	@Override
+	public void setup(EvolutionState state, Parameter base) 
+	{
+		super.setup(state, base);
+
+		Parameter p = new Parameter("gp.tc.0.init.knowledge-extraction");
+		String method = state.parameters.getString(p, null);
+		extractionMethod = KnowledgeExtractionMethod.parse(method);
+	}
+
+	@Override
+	public void finishPopulation(EvolutionState state, int result) 
+	{
+		super.finishPopulation(state, result);
+
+		FCFStatistics stats = (FCFStatistics) state.statistics;
+
+		String fileName = stats.generatePopulationFileName(stats.bestGeneration);
+
+		File file = new File(fileName);
+		if(!file.exists())
+		{
+			state.output.fatal("Generation file for extracting knowledge not found: " + fileName);
+			return;
+		}
+
+		// TODO: This is a very bad file name. CHANGE IT.
+		String outputFileName = "output.kb";
+		state.output.warning("Extracting code fragments from the file: " + fileName 
+				+ " and saving to: " + outputFileName);
+		
+		File outputFile = new File(outputFileName);
+		try(ObjectInputStream ois = new ObjectInputStream(new FileInputStream(file)); 
+				ObjectOutputStream oos = new ObjectOutputStream(new FileOutputStream(outputFile)))
+		{
+			if(outputFile.exists())
+				outputFile.delete();
+			outputFile.createNewFile();
+		
+			int nsub = ois.readInt();
+			for(int i = 0; i < nsub; i++)
+			{
+				int nind = ois.readInt();
+				for(int j = 0; j < nind; j++)
+				{
+					Object ind = ois.readObject();
+					if(!(ind instanceof GPIndividual))
+					{
+						state.output.warning("WARNING: object of instance " 
+											 + ind.getClass().toString() + "found in saved file");
+						continue;
+					}
+					extractAndSave(state, oos, (GPIndividual) ind);
+				}
+			}
+			ois.close();
+			oos.flush();
+			oos.close();
+		}
+		catch(IOException | ClassNotFoundException exp)
+		{
+			state.output.fatal("Exception occurred reading knowledge file: " + exp.toString());
+		}
+	}
+	
+	private void extractAndSave(EvolutionState state, ObjectOutputStream oos, 
+								GPIndividual gind) throws IOException
+	{		
+		ArrayList<GPNode> list = null;
+		switch(extractionMethod)
+		{
+		case AllSubtrees:
+			list = TreeSlicer.sliceAllToNodes(gind, false);
+			break;
+		case RootSubtree:
+			list = TreeSlicer.sliceRootChildrenToNodes(gind, false);
+			break;
+		default: 
+			state.output.fatal("Received an unknown extraction method: " + extractionMethod);
+		}
+		
+		if(!(state.evaluator.p_problem instanceof SimpleProblemForm))
+			state.output.fatal("GP problem is not an instance of SimpleProblemForm: " 
+								+ state.evaluator.p_problem.getClass().toString());
+		SimpleProblemForm problem = (SimpleProblemForm)state.evaluator.p_problem;
+		
+		for(GPNode node : list)
+		{
+			// Convert node to individual
+			GPIndividual newind = gind.lightClone();
+			newind.trees[0].child = node;
+			node.parent = newind.trees[0].child; 
+			
+			// Get its fitness
+			problem.evaluate(state, newind, 0, 0);
+			
+			// Save it to file
+			DoubleFittedCodeFragment cf = new DoubleFittedCodeFragment(node,
+					newind.fitness.fitness(), null);
+			oos.writeObject(cf);
+		}
+	}
+}
---
+++ src/tl/knowledge/codefragment/fitted/DoubleFittedCodeFragment.java
@@ -0,0 +1,71 @@
+package tl.knowledge.codefragment.fitted;
+
+import java.io.Serializable;
+
+import ec.Fitness;
+import ec.gp.GPIndividual;
+import ec.gp.GPNode;
+import ec.gp.GPTree;
+import tl.knowledge.codefragment.CodeFragmentKI;
+
+public class DoubleFittedCodeFragment extends CodeFragmentKI implements Serializable
+{
+	private static final long serialVersionUID = 1L;
+	Double fitnessOnSource = null;
+	Double fitnessOnTarget = null;
+
+	public DoubleFittedCodeFragment(GPNode codeFragment, Double fitnessOnTarget, 
+									Double fitnessOnSource)
+	{
+		super(codeFragment);
+		this.fitnessOnTarget = fitnessOnTarget;
+		this.fitnessOnSource = fitnessOnSource;
+	}
+
+	public DoubleFittedCodeFragment(GPNode codeFragment)
+	{
+		super(codeFragment);
+	}
+	
+	public Double getFitnessOnSource()
+	{
+		return fitnessOnSource;
+	}
+	
+	public Double getFitnessOnTarget()
+	{
+		return fitnessOnTarget;
+	}
+	
+	public void setFitnessOnTarget(double fitness)
+	{
+		this.fitnessOnTarget = fitness;
+	}
+	
+	/**
+	 * Converts this code fragment into a GP individual. This method makes a deep clone of the code
+	 * fragment that it stores so that any modifications to the returned value will not impact the 
+	 * code fragment stored here. <p>
+	 * <b>Note</b>: the fitness value of the returned object will NOT be set and therefore, any 
+	 * fitness value that may already be stored in {@code this} object will not be transfered to 
+	 * the returned value.
+	 * @param fitnessPrototype The fitness object that will be used as the prototype for creating 
+	 * the fitness of the returned code fragment.
+	 * @return a new {@code GPIndividual} object that contains this code fragment.
+	 */
+	public GPIndividual asIndividual(Fitness fitnessPrototype)
+	{
+		GPIndividual ret = new GPIndividual();
+		ret.evaluated = false;
+		ret.fitness = (Fitness) fitnessPrototype.clone();
+		ret.trees = new GPTree[1];
+		ret.trees[0] = new GPTree();
+		ret.trees[0].owner = ret;
+		GPNode item = (GPNode) getItem().clone();
+		ret.trees[0].child = item;
+		item.parent = ret.trees[0].child;
+		item.argposition = 0;
+		
+		return ret;
+	}
+}
