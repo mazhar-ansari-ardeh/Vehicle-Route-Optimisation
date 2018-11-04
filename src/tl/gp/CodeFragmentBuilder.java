1ff47a3d5c9580cdebfad9bd0eaf7af70cba4cdc
mazhar <mazhar@dunsheas-deli.ecs.vuw.ac.nz>
2018-11-04 13:21:37 +1300

    The class is marked as deprecated.

--- src/tl/gp/CodeFragmentBuilder.java
+++ src/tl/gp/CodeFragmentBuilder.java
@@ -10,6 +10,12 @@ import ec.util.Parameter;
 import tl.knowledge.KnowledgeExtractor;
 import tl.knowledge.codefragment.CodeFragmentKI;
 
+/**
+* @deprecated The class is marked as deprecated because it has design flaws and 
+* does not capture the correct method of implementing the concept of code fragment 
+* builders. 
+*/
+@Deprecated
 public abstract class CodeFragmentBuilder extends HalfBuilder
 {
 	private static final long serialVersionUID = 1L;
