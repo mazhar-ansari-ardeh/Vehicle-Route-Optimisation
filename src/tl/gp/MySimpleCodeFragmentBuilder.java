27bca530a7092bbc84cb5b50263da9da25dd2687
mazhar <mazhar@dunsheas-deli.ecs.vuw.ac.nz>
2018-12-04 16:44:01 +1300

    Created the class.

--- src/tl/gp/MySimpleCodeFragmentBuilder.java
+++ src/tl/gp/MySimpleCodeFragmentBuilder.java
@@ -1,18 +1,18 @@
 package tl.gp;
 
 import java.io.File;
-import java.io.IOException;
-import java.nio.file.Path;
 
 import ec.EvolutionState;
 import ec.gp.*;
 import ec.gp.koza.HalfBuilder;
 import ec.util.Parameter;
+import tl.TLLogger;
+import tl.knowledge.KnowledgeExtractionMethod;
 import tl.knowledge.KnowledgeExtractor;
 import tl.knowledge.codefragment.CodeFragmentKI;
 import tl.knowledge.codefragment.simple.MySimpleCodeFragmentKB;
 
-public class MySimpleCodeFragmentBuilder extends HalfBuilder
+public class MySimpleCodeFragmentBuilder extends HalfBuilder implements TLLogger<GPNode>
 {
 	private static final long serialVersionUID = 1L;
 
@@ -38,11 +38,14 @@ public class MySimpleCodeFragmentBuilder extends HalfBuilder
 	 */
 	private int transferPercent;
 
+	private static int cfCounter = 0;
 
 	@Override
 	public void setup(EvolutionState state, Parameter base)
 	{
 		super.setup(state, base);
+
+		knowledgeSuccessLogID = setupLogger(state, base);
 
 		Parameter knowledgeFileName = base.push(P_KNOWLEDGE_FILE);
 		String fileName = state.parameters.getString(knowledgeFileName, null);
@@ -55,11 +58,6 @@ public class MySimpleCodeFragmentBuilder extends HalfBuilder
 		{
 			state.output.fatal("Knowledge file does not exist: " + fileName, knowledgeFileName);
 		}
-
-//		Parameter problemParam = new Parameter("pop.subpop.0.species.fitness");
-//		Fitness fitness = (Fitness) state.parameters.getInstanceForParameter(problemParam,
-//				null, Fitness.class);
-//		fitness.setup(state, problemParam);
 
 		Parameter knowledgeExtraction = base.push(P_KNOWLEDGE_EXTRACTION);
 		String extraction = state.parameters.getString(knowledgeExtraction, null);
@@ -75,67 +73,29 @@ public class MySimpleCodeFragmentBuilder extends HalfBuilder
 		state.output.warning("MYSimpleCodeFragmentBuilder loaded. Transfer percent: "
 							 + transferPercent + ", extraction method: " + extractionMethod
 							 + ", transfer percent: " + transferPercent);
-
-		try {
-			Parameter knowledgeLogFileNameParam = base.push(P_KNOWLEDGE_LOG_FILE_NAME);
-			String knowledgeLogFile = state.parameters.getString(knowledgeLogFileNameParam, null);
-			File successKnLog = new File(knowledgeLogFile + ".succ.log");
-			if(successKnLog.exists())
-				successKnLog.delete();
-
-			Path pathToSuccFile = successKnLog.toPath();
-			Path pathToSuccDir = pathToSuccFile.getParent();
-			if(pathToSuccDir != null)
-			{
-				File statDirFile = pathToSuccDir.toFile();
-				if(statDirFile.exists() == false && statDirFile.mkdirs() == false)
-					state.output.fatal("Failed to create stat directory: "
-									   + pathToSuccDir.toString());
-			}
-
-			Path pathToFailFile = successKnLog.toPath();
-			Path pathToFailDir = pathToFailFile.getParent();
-			if(pathToFailDir != null)
-			{
-				File statDirFile = pathToFailDir.toFile();
-				if(statDirFile.exists() == false && statDirFile.mkdirs() == false)
-					state.output.fatal("Failed to create stat directory: "
-									   + pathToFailDir.toString());
-			}
-
-			successKnLog.createNewFile();
-
-			knowledgeSuccessLogID = state.output.addLog(successKnLog, false);
-		}
-		 catch (IOException e) {
-				state.output.fatal("Failed to create knowledge log file in CodeFragmentBuilder: "
-						+ e.getStackTrace().toString());
-			}
 	}
 
 
-	private int transferCount = 0;
 	public GPNode newRootedTree(final EvolutionState state, final GPType type, final int thread,
 			final GPNodeParent parent, final GPFunctionSet set,	final int argposition,
 			final int requestedSize)
 	{
 		int popSize = state.parameters.getInt(new Parameter("pop.subpop.0.size"), null);
 		int numToTransfer = Math.round(popSize * transferPercent / 100f);
-		if(numToTransfer >= 0 && transferCount < numToTransfer)
+		if(numToTransfer >= 0 && cfCounter < numToTransfer)
 		{
 			CodeFragmentKI cf = (CodeFragmentKI) extractor.getNext();
 			if(cf != null)
 			{
 				cfCounter++;
-				log(state, cf, knowledgeSuccessLogID);
+				log(state, cf, cfCounter, knowledgeSuccessLogID);
 				GPNode node = cf.getItem();
 				node.parent = parent;
-				transferCount++;
 				// System.out.println("Loaded a CF: " + node.makeCTree(false, false, false));
 				return node;
 			}
 			else
-				log(state, null, knowledgeSuccessLogID);
+				log(state, null, cfCounter, knowledgeSuccessLogID);
 	//		else
 	//			System.out.println("CF is null");
 		}
@@ -145,12 +105,4 @@ public class MySimpleCodeFragmentBuilder extends HalfBuilder
 			return fullNode(state,0,state.random[thread].nextInt(maxDepth-minDepth+1) + minDepth,type,thread,parent,argposition,set);
 	}
 
-	private static int cfCounter = 0;
-
-	private void log(EvolutionState state, CodeFragmentKI it, int logID)
-	{
-		state.output.println(cfCounter + ": \t" + (it == null ? "null" : it.toString()), logID);
-		state.output.flush();
-		state.output.println("", logID);
-	}
 }
