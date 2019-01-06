25080d7bf040d3c779c29dee27f49226cafa05ef
Mazhar Ansari Ardeh <mazhar@dunsheas-deli.ecs.vuw.ac.nz>
2019-01-06 17:47:13 +1300

    Save the changes

--- RunExperiment/processfile.py
+++ RunExperiment/processfile.py
@@ -70,7 +70,19 @@ import matplotlib.pyplot as plt
 
 #     out.close()
 
-dirpath = '/vol/grid-solar/sgeusers/mazhar/val9C-v6-to7-depthed-frequent/'
+# dirpath = '/vol/grid-solar/sgeusers/mazhar/val9C-v5-to6/'
+experiments = ['gdb1-v5-to4', 'gdb1-v5-to6', 'gdb2-v6-to5', 'gdb2-v6-to7'
+                            # , 'gdb9-v10-to11'
+                            , 'gdb21-v6-to5'
+                            , 'gdb21-v6-to7'
+                            # , 'gdb8-v10-to9', 'gdb8-v10-to11', 'gdb9-v10-to9'
+                            # , 'gdb23-v10-to9', 'gdb23-v10-to11', 'val9C-v5-to4', 'val9C-v5-to6', 'val9D-v10-to9', 'val9D-v10-to11'
+                            # , 'val10C-v5-to4'
+                            # , 'val10C-v5-to6'
+                            # , 'val10D-v10-to9', 'val10D-v10-to11'
+                            ]
+dirbase = Path('/home/mazhar/scratch/NewCEC/')
+dirpath = dirbase / 'val10D-v10-to9'
 generations = 50
 
 def bin_to_txt_pop(path_to_folder, print_output = True):
@@ -171,7 +183,7 @@ def load_csv(file, comma=','):
 
     return csv_matrix
 
-def iterate_grid_output(experiment_path):
+def plot_grid_output(experiment_path):
 
     # Folder structure is 'experiment_path/run/stats/algorithm/test'
 
@@ -179,23 +191,7 @@ def iterate_grid_output(experiment_path):
     experiment_path = Path(experiment_path)
     (_, runs, _) = next(os.walk(experiment_path))
 
-    gen_mean_tr = {}
     gen_mean_ts = {}
-
-    # experiment_tr_fitnesses = {}
-    experiment_ts_fitnesses = {}
-
-    def update_gen_mean_tr():
-        nonlocal gen_mean_tr
-        csv = load_csv(experiment_path / run / 'stats' / algorithm / 'population.gen.statistics')
-        if not algorithm in gen_mean_tr:
-            gen_mean_tr[algorithm] = {}
-        for gen in range(generations):
-            if not gen in gen_mean_tr[algorithm]:
-                gen_mean_tr[algorithm][gen] = {}
-            gen_mean_tr[algorithm][gen][int(run)] = float(csv[gen][3])
-            # if gen == 9 and ('wok' in algorithm):
-            #     print(run, ': ', float(csv[gen][3]))
 
     def update_gen_mean_ts(file):
         nonlocal gen_mean_ts
@@ -209,42 +205,96 @@ def iterate_grid_output(experiment_path):
             # if 'wok' in algorithm and gen == 7:
             #     print(run, ':', gen_mean_ts[algorithm][gen][int(run)])
 
-
-    def checkKnowTrace():
-        # tree_regex = r'\[([A-Z]|\s|[a-z]|[+\-*/]|\(|\)|\d|\.)+'
-
-        if 'wok' in algorithm:
-            return
-
-        (_, _, files) = next(os.walk(experiment_path / run / 'stats' / algorithm))
-        knowFile = [file for file in files if file.endswith('.log')]
-        if not knowFile:
-            print('Could not file knowledge file', algorithm)
-            return 
-        knowFile = knowFile[0]
-
-        knowledge_items = []
-        for line in open(experiment_path / run/ 'stats' / algorithm / knowFile):
-            if 'null' in line or not line.strip():
-                continue
-
-            orig_line = line
-            line = line.split(',')[0]
-            line = line[line.find('[') + 1:]
-            line = line.lstrip('(').rstrip(')')
-            knowledge_items.append((line, orig_line))
-
-        for a_file in files:
-            if not a_file.endswith('.ind'):
-                continue 
-
-            for line in open(experiment_path / run/ 'stats' / algorithm / a_file):
-                for item in knowledge_items:
-                    if item[0] in line:
-                        print('Found: ', item[1], 'in\n', line, 'of', algorithm + '/' + a_file, '\n')
-
+    def plot_mean_of_genbest_tr(gen_mean):
+        markers = (marker for marker in ['.', ',', 'o', 'v', '^', '<', '>'
+                                            , '1', '2', '3', '4', 's', 'p', '*', 'h', 'H', '+', 'x'
+                                            , 'D', 'd', '|', '_'])
+        line_styles = ['--', '-.', ':']
+        sc = 0
         
+        # figsize=(width, height)
+        fig_all = plt.figure(figsize=(60, 32))
+        ax_all = fig_all.add_subplot(111)
+        ax_all.set_xlabel('Generation')
+        ax_all.set_ylabel('Fitness')
+        
+        for algorithm in sorted(gen_mean):
+            box_data = []
+            mean_data = []
+            for gen in gen_mean[algorithm]:
+                box_data.append((list(gen_mean[algorithm][gen].values())))
+                mean_data.append(statistics.mean(list(gen_mean[algorithm][gen].values())))
+                # print(algorithm, ', ', gen, ': ', statistics.mean(list(gen_mean[algorithm][gen].values())))
 
+            fig = plt.figure(figsize=(12,14))
+            ax = fig.add_subplot(311)
+            ax.set_title(algorithm)
+            ax.set_xlabel('Generation')
+            ax.set_ylabel('Fitness')
+            # ax.boxplot(box_data)
+
+            ax = fig.add_subplot(312)
+            ax.set_title(algorithm + ': average of 30 runs per generation')
+            ax.set_xlabel('Generation')
+            ax.set_ylabel('Average fitness')
+            # ax.plot(range(1, len(mean_data) + 1), mean_data)
+            if not 'writeknow' in algorithm:
+                label = algorithm.replace('Knowledge', '')\
+                                    .replace('depthed_frequent', 'FrequentSub')\
+                                    .replace('Root', 'root')\
+                                    .replace('wok', 'Without Transfer')\
+                                    .split('-wk-')[-1]
+                label = label[0].upper() + label[1:]
+
+                if 'wok' in algorithm:
+                    ax_all.plot(range(1, len(mean_data) + 1), mean_data, label="Without Transfer", color='k')
+                else:
+                    sc += 1 
+                    sc %= 3 
+
+                    ax_all.plot(range(1, len(mean_data) + 1), mean_data, label=label
+                                       , linestyle=line_styles[sc], marker=next(markers), markersize=10)
+
+            ax = fig.add_subplot(313)
+            ax.set_title(algorithm + ': combo')
+            ax.set_xlabel('Generation')
+            ax.set_ylabel('Fitness')
+            # ax.boxplot(box_data)
+            # ax.plot(range(1, len(mean_data) + 1), mean_data)
+            if not Path(experiment_path.name).exists(): 
+                Path(experiment_path.name).mkdir()
+            # fig.savefig(f'{experiment_path.name + "/" + algorithm}.jpg')
+            print('Saved', f'{experiment_path.name + "/" + algorithm}.jpg')
+        output_folder = Path('/home/mazhar/MyPhD/MyPapers/Generalizability of GPTL/')
+        ax_all.legend(fontsize=48)
+        # fig_all.savefig(experiment_path.name + "/" + algorithm.split('-')[0] + '-all.jpg')
+        fig_all.savefig(output_folder / (experiment_path.name + '-all.jpg'))
+
+    for run in runs:
+        if run.isnumeric() == False:
+            continue
+        # Now inside 'experiment_path/run'
+        
+        (_, algorithms, _) = next(os.walk(experiment_path / run / 'stats'))
+        for algorithm in algorithms:
+            # Now inside 'experiment_path/run/stats/algorithm'
+
+            if (experiment_path / run / 'stats' / algorithm / 'test').exists():
+                # Now inside 'experiment_path/run/stats/algorithm/test'
+
+                (_, _, test_files) = next(os.walk(experiment_path / run / 'stats' / algorithm / 'test'))
+                for test_file in test_files:
+                    if not test_file.endswith('.csv'):
+                        continue
+                    update_gen_mean_ts(experiment_path / run / 'stats' / algorithm / 'test' / test_file)
+
+    plot_mean_of_genbest_tr(gen_mean_ts)
+    print('Finished')
+
+
+def get_experiment_stats(experiment_path):
+
+    experiment_ts_fitnesses = {}
     def get_exp_stats():
         nonlocal experiment_ts_fitnesses
 
@@ -265,6 +315,7 @@ def iterate_grid_output(experiment_path):
         # experiment_tr_fitnesses[algorithm][run] = tr_fit
         experiment_ts_fitnesses[algorithm][run] = ts_fit
 
+
     def save_exp_stats():
         wok_exp = ''
         for algorithm in experiment_ts_fitnesses:
@@ -276,16 +327,15 @@ def iterate_grid_output(experiment_path):
             Path(experiment_path.name).mkdir()
 
         csv_file = open(f'{experiment_path.name + "/" + "stats"}.csv', 'w')
-        csv_file.write('Algorithm, Min, Mean, Stdev, WilcoxPVal\n')
+        latex_file = open(f'{experiment_path.name + "/" + "stats"}.tex', 'w')
 
-        for algorithm in experiment_ts_fitnesses:
-            run_values = experiment_ts_fitnesses[algorithm].values()
-            print(algorithm, run_values)
-            min_val = min(run_values)
-            # for ind in experiment_ts_fitnesses[algorithm].keys():
-            #     if min_val == experiment_ts_fitnesses[algorithm][ind]:
-            #         min_ind = ind
-            #         break
+        csv_file.write('Algorithm, Best, Mean, Stdev, WilcoxPVal\n')
+        latex_file.write(r'\begin{table}[]' + '\n'
+                       + r'\resizebox{\columnwidth}{!}{%'  + '\n'
+                       + r'\begin{tabular}{lllll} \hline'  + '\n'
+                       + r'Algorithm & Best               & Mean               & Stdev              & WilcoxPVal           \\ \hline' + '\n')
+
+        for algorithm in sorted(experiment_ts_fitnesses):
             miin = min(experiment_ts_fitnesses[algorithm].values())
             mean = statistics.mean(experiment_ts_fitnesses[algorithm].values())
             stdev = statistics.stdev(experiment_ts_fitnesses[algorithm].values())
@@ -293,74 +343,39 @@ def iterate_grid_output(experiment_path):
                 pval = stats.wilcoxon(list(experiment_ts_fitnesses[algorithm].values()), list(experiment_ts_fitnesses[wok_exp].values()))[1]
             else:
                 pval = '--'
-            if 'writeknow' in algorithm:
+            # if 'writeknow' in algorithm:
+            #     continue
+
+            if 'wok' in algorithm:
+                algorithm = 'Without Transfer'
+            elif 'writeknow' in algorithm:
                 continue
+            else:
+                algorithm = algorithm.split('-wk-')[-1]
+                algorithm = algorithm[0].upper() + algorithm[1:]
 
             csv_file.write(f'{algorithm}, {miin}, {mean}, {stdev}, {pval}\n')
+            algorithm.replace('_', '\\_')
+            latex_file.write(f'{algorithm} & {miin} & {mean} & {stdev} & {pval}\\\\\n')
 
         csv_file.close()
+        latex_file.write(r'\end{tabular}' + '\n'
+                       + r'}' + '\n'
+                       + r'\caption{caption}' + '\n'
+                       + r'\end{table}')
+        latex_file.close()
 
-    def plot_mean_of_genbest_tr(gen_mean):
-        # figsize=(width, height)
-        fig_all = plt.figure(figsize=(200, 32))
-        ax_all = fig_all.add_subplot(111)
-        ax_all.set_xlabel('Generation')
-        ax_all.set_ylabel('Fitness')
-        
-        for algorithm in sorted(gen_mean):
-            box_data = []
-            mean_data = []
-            for gen in gen_mean[algorithm]:
-                box_data.append((list(gen_mean[algorithm][gen].values())))
-                mean_data.append(statistics.mean(list(gen_mean[algorithm][gen].values())))
-                # print(algorithm, ', ', gen, ': ', statistics.mean(list(gen_mean[algorithm][gen].values())))
-
-            fig = plt.figure(figsize=(12,14))
-            ax = fig.add_subplot(311)
-            ax.set_title(algorithm)
-            ax.set_xlabel('Generation')
-            ax.set_ylabel('Fitness')
-            ax.boxplot(box_data)
-
-            ax = fig.add_subplot(312)
-            ax.set_title(algorithm + ': average of 30 runs per generation')
-            ax.set_xlabel('Generation')
-            ax.set_ylabel('Average fitness')
-            ax.plot(range(1, len(mean_data) + 1), mean_data)
-            if not 'writeknow' in algorithm:
-                if 'wok' in algorithm:
-                    ax_all.plot(range(1, len(mean_data) + 1), mean_data, label=algorithm, color='k')
-                else:
-                    ax_all.plot(range(1, len(mean_data) + 1), mean_data, label=algorithm)
-
-            ax = fig.add_subplot(313)
-            ax.set_title(algorithm + ': combo')
-            ax.set_xlabel('Generation')
-            ax.set_ylabel('Fitness')
-            ax.boxplot(box_data)
-            ax.plot(range(1, len(mean_data) + 1), mean_data)
-            if not Path(experiment_path.name).exists(): 
-                Path(experiment_path.name).mkdir()
-            fig.savefig(f'{experiment_path.name + "/" + algorithm}.jpg')
-            print('Saved', f'{experiment_path.name + "/" + algorithm}.jpg')
-        ax_all.legend()
-        fig_all.savefig(experiment_path.name + "/" +'all.jpg')
-
-
+    experiment_path = Path(experiment_path)
+    (_, runs, _) = next(os.walk(experiment_path))
 
     for run in runs:
         if run.isnumeric() == False:
             continue
         # Now inside 'experiment_path/run'
-        
 
-        (_, algorithms, files) = next(os.walk(experiment_path / run / 'stats'))
+        (_, algorithms, _) = next(os.walk(experiment_path / run / 'stats'))
         for algorithm in algorithms:
             # Now inside 'experiment_path/run/stats/algorithm'
-            # checkKnowTrace()
-
-            # update_gen_mean_tr()
-            # bin_to_txt_pop(experiment_path/run/'stats'/algorithm)
 
             if (experiment_path / run / 'stats' / algorithm / 'test').exists():
                 # Now inside 'experiment_path/run/stats/algorithm/test'
@@ -370,45 +385,43 @@ def iterate_grid_output(experiment_path):
                     if not test_file.endswith('.csv'):
                         continue
                     get_exp_stats()
-                    update_gen_mean_ts(experiment_path / run / 'stats' / algorithm / 'test' / test_file)
-
-    # Iteration finished. Do any processing here
-
-    save_exp_stats()
-    # plot_mean_of_genbest_tr(gen_mean_ts)
-    print('Finished')
 
 
+    save_exp_stats()                
 
 
+def plot_best_of_algorithms(experiment_path):
+    experiment_path = Path(experiment_path)
+    (_, runs, _) = next(os.walk(experiment_path))
+
+    best_run_ind = runs[0]
+    best_run_fit = float('inf')
+
+    for run in runs:
+        if run.isnumeric() == False:
+            continue
+        # Now inside 'experiment_path/run'
+        
+
+        (_, algorithms, _) = next(os.walk(experiment_path / run / 'stats'))
+        for algorithm in algorithms:
+            # Now inside 'experiment_path/run/stats/algorithm'
+
+            if (experiment_path / run / 'stats' / algorithm / 'test').exists():
+                # Now inside 'experiment_path/run/stats/algorithm/test'
+
+                (_, _, test_files) = next(os.walk(experiment_path / run / 'stats' / algorithm / 'test'))
+                for test_file in test_files:
+                    if not test_file.endswith('.csv'):
+                        continue
 
 
-
-
-
-
-
-
-
+                csv_matrix = load_csv(experiment_path / run / 'stats' / algorithm / 'test' / test_file)
+                ts_fit = float(csv_matrix[-1][7])
 
 
 if __name__ == '__main__':
     # process_grid_output(dirpath)
-    iterate_grid_output(dirpath)
-
-    # subtree50= \
-    # "/vol/grid-solar/sgeusers/mazhar/gdb8-v10-to11/{run}/stats/gdb8-v10-to-11-wk-subtree50/"
-    # wok_template = '/vol/grid-solar/sgeusers/mazhar/gdb8-v10-to11/{run}/stats/gdb8-v11-wok/'
-    # s50 = []
-    # wok = []
-    # for run in range(1, 31):
-    #     path = subtree50.format(run=run)
-    #     _, fitness = process_csv(path + 'test/total-cost-gdb8-11-0.2-0.2.csv')
-    #     s50.append(fitness)
-    #
-    #     path = wok_template.format(run=run)
-    #     _, fitness = process_csv(path + 'test/total-cost-gdb8-11-0.2-0.2.csv')
-    #     wok.append(fitness)
-    #
-    # print(statistics.mean(wok), statistics.stdev(wok))
-    # print(statistics.mean(s50), statistics.stdev(s50))
+    for exp in experiments:
+        plot_grid_output(dirbase / exp)
+        get_experiment_stats(dirbase / exp)
