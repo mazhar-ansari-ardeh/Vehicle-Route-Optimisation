eda907f39bc845c818ba5a86bda51839b9b53148
Mazhar <mazhar.ansari.ardeh@gmail.com>
2019-07-14 21:23:18 +1200

    Implemented the observer pattern into the class so that after a simplification, a handler can be invoked

--- src/tl/gp/simplification/TreeSimplifier.java
+++ src/tl/gp/simplification/TreeSimplifier.java
@@ -3,9 +3,13 @@ package tl.gp.simplification;
 import ec.EvolutionState;
 import ec.gp.GPIndividual;
 
+import java.util.function.Consumer;
+
 public class TreeSimplifier
 {
     protected TreeSimplifier next = null;
+
+    Consumer<GPIndividual> simplifiedEvent = null;
 
     // Provided a basic implementation so that the class can be concrete and therefore, used to create variables
     // without needing to use a different class to do it.
@@ -18,11 +22,19 @@ public class TreeSimplifier
     {
         // This implementation does not check for replications in the chain so that multiple simplifiers can be chained
         // together.
+       return setNext(nextSimplifier, null);
+    }
+
+    public TreeSimplifier setNext(TreeSimplifier nextSimplifier, Consumer<GPIndividual> simplifiedEvent)
+    {
+        // This implementation does not check for replications in the chain so that multiple simplifiers can be chained
+        // together.
         TreeSimplifier lastOne = this;
         while (lastOne.next != null)
             lastOne = lastOne.next;
 
         lastOne.next = nextSimplifier;
+        lastOne.next.simplifiedEvent = simplifiedEvent;
 
         return this;
     }
@@ -30,6 +42,9 @@ public class TreeSimplifier
     public boolean simplifyTree(EvolutionState state, GPIndividual ind)
     {
         boolean retval = simplify(state, ind);
+        if(simplifiedEvent != null)
+            simplifiedEvent.accept(ind);
+
         if (next != null)
             retval |= next.simplifyTree(state, ind);
 
