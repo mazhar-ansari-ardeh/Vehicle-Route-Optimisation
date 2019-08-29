cb7dc94c5080ec23eb483c960aaffda311ebb490
mzhr <mazhar.ansari.ardeh@gmail.com>
2019-08-29 16:33:50 +1200

    Created the 'ecj' package.

---
+++ src/tl/ecj/ECJUtils.java
@@ -0,0 +1,52 @@
+package tl.ecj;
+
+import ec.Evaluator;
+import ec.EvolutionState;
+import ec.Evolve;
+import ec.util.Parameter;
+import ec.util.ParameterDatabase;
+
+import java.util.ArrayList;
+
+/**
+ * Utility class to help working with ECJ.
+ */
+public class ECJUtils
+{
+
+	/**
+	 * Loads an instance of ECJ with the parameter file given and also with the extra parameters given variable argument
+	 * list
+	 * @param paramFileNamePath The path (containing name) of the parameter file to load.
+	 * @param ecjParams Extra parameters to pass to ECJ. The string items in this list cannot contain space characters
+	 *                  and do not require the '-p' directive that is required to pass parameters to ECJ from command
+	 *                  line.
+	 * @return an object of {@code EvolutionState}.
+	 */
+	public static EvolutionState loadECJ(String paramFileNamePath, String... ecjParams)
+	{
+		ArrayList<String> params = new ArrayList<>();
+		params.add("-file");
+		params.add(paramFileNamePath);
+		for(String param : ecjParams)
+		{
+			params.add("-p");
+			params.add(param);
+		}
+		String[] processedParams = new String[params.size()];
+		params.toArray(processedParams);
+		ParameterDatabase parameters = Evolve.loadParameterDatabase(processedParams);
+
+		EvolutionState state = Evolve.initialize(parameters, 0);
+
+		Parameter p;
+
+		// setup the evaluator, essentially the test evaluation model
+		p = new Parameter(EvolutionState.P_EVALUATOR);
+		state.evaluator = (Evaluator)
+				(parameters.getInstanceForParameter(p, null, Evaluator.class));
+		state.evaluator.setup(state, p);
+
+		return state;
+	}
+}
---
+++ src/tl/ecj/package-info.java
@@ -0,0 +1,4 @@
+/**
+ * This package contains utilities that can help ease the pain of working with ECJ.
+ */
+package tl.ecj;
