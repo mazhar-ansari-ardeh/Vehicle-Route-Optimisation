60573fd1d29585d3a23f5ffe3caef33f4b24e93e
Mazhar <mazhar.ansari.ardeh@gmail.com>
2019-09-04 11:00:16 +1200

    Documented the interface 'HashCalculator'. The default 'hashOf' method is changed so that GP tree can be selected through its input arguments.

--- src/tl/gp/hash/HashCalculator.java
+++ src/tl/gp/hash/HashCalculator.java
@@ -3,11 +3,25 @@ package tl.gp.hash;
 import ec.gp.GPIndividual;
 import ec.gp.GPNode;
 
+/**
+ * Defines the interface for any hash calculating algorithm.
+ */
 public interface HashCalculator
 {
+    /**
+     * Calculates the hash value of a GP (sub-)tree whose root node is given.
+     * @param tree the root node of a GP (sub-)tree
+     * @return the hash value of the given GP (sub-)tree
+     */
     int hashOfTree(GPNode tree);
 
-    default int hashOf(GPIndividual ind)
+    /**
+     * Calculates the hash value of a GP tree of the given GP individual.
+     * @param ind the GP individual that contains GP trees
+     * @param tree the index of the GP tree of the given GP individual
+     * @return the hash value of the desired GP tree of the given individual
+     */
+    default int hashOf(GPIndividual ind, int tree)
     {
         if(ind == null || ind.trees == null)
             throw new NullPointerException("The individual or its trees cannot be null");
@@ -15,6 +29,6 @@ public interface HashCalculator
         if(ind.trees.length != 1)
             throw new IllegalArgumentException("The method currently supports only individuals with 1 trees");
 
-        return hashOfTree(ind.trees[0].child);
+        return hashOfTree(ind.trees[tree].child);
     }
 }
