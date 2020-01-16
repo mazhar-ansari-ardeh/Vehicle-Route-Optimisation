108b3b883d7deaf3684eb26f73e190e12f1bbbd1
Mazhar <mazhar.ansari.ardeh@gmail.com>
2020-01-17 10:53:21 +1300

    Added a warning comment: This implementation does not consider the reference rule.

--- src/tl/gp/niching/PhenoCharacterisation.java
+++ src/tl/gp/niching/PhenoCharacterisation.java
@@ -2,61 +2,48 @@ package tl.gp.niching;
 
 
 import gphhucarp.core.Arc;
-import gphhucarp.decisionprocess.DecisionProcess;
+//import gphhucarp.decisionprocess.DecisionProcess;
 import gphhucarp.decisionprocess.RoutingPolicy;
 import gphhucarp.decisionprocess.reactive.ReactiveDecisionSituation;
 
-import java.util.ArrayList;
+//import java.util.ArrayList;
 import java.util.List;
 
 public class PhenoCharacterisation
 {
     private RoutingPolicy rp;
     private RoutingPolicy referenceRule;
-    private ArrayList<ReactiveDecisionSituation> decisionSituations;
+    private List<ReactiveDecisionSituation> decisionSituations;
     private int[] referenceIndexes;
-    private DecisionProcess decisionProcess;
+//    private DecisionProcess decisionProcess;
 
-    public PhenoCharacterisation(final ArrayList<ReactiveDecisionSituation> dps, final RoutingPolicy policy) {
-        this.decisionProcess = null;
+    public PhenoCharacterisation(final List<ReactiveDecisionSituation> dps, final RoutingPolicy referenceRule) {
+//        this.decisionProcess = null;
         this.decisionSituations = dps;
-        this.referenceRule = policy;
+        this.referenceRule = referenceRule;
         this.referenceIndexes = new int[this.decisionSituations.size()];
         this.calcReferenceIndexes();
     }
 
-    public List<ReactiveDecisionSituation> getDecisionSituations() {
-        return this.decisionSituations;
-    }
+//    public List<ReactiveDecisionSituation> getDecisionSituations() {
+//        return this.decisionSituations;
+//    }
+//
+//    public RoutingPolicy getReferenceRule() {
+//        return this.referenceRule;
+//    }
 
-    public RoutingPolicy getReferenceRule() {
-        return this.referenceRule;
-    }
-
-    public int[] getReferenceIndexes() {
-        return this.referenceIndexes;
-    }
-
-    private void calcReferenceIndexes() {
-        for (int i = 0; i < this.decisionSituations.size(); ++i) {
-            final ReactiveDecisionSituation situation = this.decisionSituations.get(i);
-            final Arc op = this.referenceRule.next(situation);
-            final int index = situation.getPool().indexOf(op);
-            this.referenceIndexes[i] = index;
-        }
-    }
-
-    public void setReferenceRule(final RoutingPolicy rule) {
-        this.referenceRule = rule;
-        this.calcReferenceIndexes();
-    }
+//    public void setReferenceRule(final RoutingPolicy rule) {
+//        this.referenceRule = rule;
+//        this.calcReferenceIndexes();
+//    }
 
     public int[] characterise(final RoutingPolicy rule) {
         final int[] charList = new int[this.decisionSituations.size()];
-        for (int i = 0; i < this.decisionSituations.size(); ++i) {
+        for (int i = 0; i < this.decisionSituations.size(); i++) {
             final ReactiveDecisionSituation situation = this.decisionSituations.get(i);
             final List<Arc> queue = (List<Arc>)situation.getPool();
-            final int refIdx = this.referenceIndexes[i];
+            final int refIdx = this.referenceIndexes[i]; // Warning: This implementation does not consider the reference rule
             final Arc op = rule.next(situation);
             final int rank = situation.getPool().indexOf(op);
             charList[i] = rank;
@@ -72,4 +59,25 @@ public class PhenoCharacterisation
         }
         return Math.sqrt(distance);
     }
+
+    private void calcReferenceIndexes() {
+        for (int i = 0; i < this.decisionSituations.size(); ++i) {
+            final ReactiveDecisionSituation situation = this.decisionSituations.get(i);
+            final Arc op = this.referenceRule.next(situation);
+            final int index = situation.getPool().indexOf(op);
+            this.referenceIndexes[i] = index;
+        }
+    }
+
+//    public List<ReactiveDecisionSituation> getDecisionSituations() {
+//        return this.decisionSituations;
+//    }
+//
+//    public RoutingPolicy getReferenceRule() {
+//        return this.referenceRule;
+//    }
+//
+//    public int[] getReferenceIndexes() {
+//        return this.referenceIndexes;
+//    }
 }
