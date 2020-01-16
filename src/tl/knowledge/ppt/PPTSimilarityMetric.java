f797e65cc036afc99c402d3213d34b9913e7f1df
Mazhar <mazhar.ansari.ardeh@gmail.com>
2020-01-17 11:19:09 +1300

    An interface for defining various similarity measures.

--- src/tl/knowledge/ppt/PPTSimilarityMetric.java
+++ src/tl/knowledge/ppt/PPTSimilarityMetric.java
@@ -1,5 +1,21 @@
-package tl.knowledge.ppt.pipe;
+package tl.knowledge.ppt;
 
+import ec.gp.GPNode;
+import tl.knowledge.ppt.pipe.PPTree;
+
+
+/**
+ * This interface encapsulates the similarity measure between a PPT and a GP individual.
+ */
 public interface PPTSimilarityMetric
 {
+    /**
+     * Measures how similar a given GP individual, {@code tree} is to a given PPT. A good similarity measure should give a
+     * high score to GP individuals that are very likely to be generated from the given PPT.
+     * @param ppt a {@code PPTree} object.
+     * @param tree a GP tree to be measure its similarity to the given PPT.
+     * @return a real-valued number that indicates the level of similarity. Lower values of this number indicate low
+     * similarity and higher-values indicate higher similarity.
+     */
+    double similarity(PPTree ppt, GPNode tree);
 }
