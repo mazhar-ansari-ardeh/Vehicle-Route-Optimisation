bdf39a6722144886f7bf54fbc674b7b5194b2592
mzhr <mazhar.ansari.ardeh@gmail.com>
2020-05-09 13:22:16 +1200

    Added the ability to enable/disable DMS saving. Added the 'setDMSSavingEnabled' method.

--- src/tl/gphhucarp/dms/DMSSavingGPHHState.java
+++ src/tl/gphhucarp/dms/DMSSavingGPHHState.java
@@ -1,29 +1,60 @@
 package tl.gphhucarp.dms;
 
+import ec.EvolutionState;
 import ec.Individual;
+import ec.util.Parameter;
 import gphhucarp.decisionprocess.DecisionSituation;
 import gphhucarp.decisionprocess.reactive.ReactiveDecisionSituation;
 import gphhucarp.gp.GPHHEvolutionState;
-import tl.gp.niching.SimpleNichingAlgorithm;
-import tl.gphhucarp.dms.DMSSaver;
 
 import java.util.*;
 
 public class DMSSavingGPHHState extends GPHHEvolutionState implements DMSSaver
 {
-	private TreeMap<Individual, List<DecisionSituation>> seenSituations = new TreeMap<>();
+	protected TreeMap<Individual, List<DecisionSituation>> seenSituations = new TreeMap<>();
 
 	private List<ReactiveDecisionSituation> initialSituations = new ArrayList<>();
+	private boolean saveDMS = true;
 
 	@Override
-	public void resetSeenSituations()
+	public void setup(EvolutionState state, Parameter base)
 	{
-		seenSituations.clear();
+		super.setup(state, base);
+	}
+
+//	@Override
+//	public void resetSeenSituations()
+//	{
+//		seenSituations.clear();
+//	}
+
+	private void updateDSMIfBetter(Individual ind, List<DecisionSituation> situations)
+	{
+		if(!this.saveDMS)
+			return;
+		final int MAX_SIZE = 2;
+		if(seenSituations.size() < MAX_SIZE)
+		{
+			List<DecisionSituation> clonedSituations = new ArrayList<>(situations.size());
+			situations.forEach(situation -> clonedSituations.add(new ReactiveDecisionSituation((ReactiveDecisionSituation) situation)));
+			seenSituations.put(ind, clonedSituations);
+			return;
+		}
+		if(ind.fitness.fitness() < seenSituations.lastKey().fitness.fitness())
+		{
+			List<DecisionSituation> clonedSituations = new ArrayList<>(situations.size());
+			situations.forEach(situation -> clonedSituations.add(new ReactiveDecisionSituation((ReactiveDecisionSituation) situation)));
+			seenSituations.put(ind, clonedSituations);
+			seenSituations.remove(seenSituations.lastKey());
+		}
 	}
 
 	@Override
 	public void updateSeenSituations(Individual ind, List<DecisionSituation> situations)
 	{
+		if(!this.saveDMS)
+			return;
+
 		if(seenSituations.size() > 1)
 			return;
 
@@ -80,25 +111,35 @@ public class DMSSavingGPHHState extends GPHHEvolutionState implements DMSSaver
 		this.initialSituations.add((ReactiveDecisionSituation) initialSituation);
 	}
 
+	@Override
+	public void setDMSSavingEnabled(boolean enabled) {
+		this.saveDMS = enabled;
+	}
+
+	@Override
+	public boolean isDMSSavingEnabled() {
+		return this.saveDMS;
+	}
+
 	protected void clear()
 	{
-		if(!clear || seenSituations.size() == 0)
-		{
-			seenSituations.clear();
-			return;
-		}
-
-		int numDecisionSituations = 30;
-		long shuffleSeed = 8295342;
-
-		List<ReactiveDecisionSituation> allSeenSituations = getAllSeenSituations();
-		Collections.shuffle(allSeenSituations, new Random(shuffleSeed));
-		allSeenSituations = allSeenSituations.subList(0, numDecisionSituations);
-
-		SimpleNichingAlgorithm.clearPopulation(this, allSeenSituations, 0, 1);
-		allSeenSituations.clear();
-
-		// Clear the list so that new situations do not pile on the old ones. Don't know if this is a good idea.
-		seenSituations.clear();
+//		if(!clear || seenSituations.size() == 0)
+//		{
+//			seenSituations.clear();
+//			return;
+//		}
+//
+//		int numDecisionSituations = 30;
+//		long shuffleSeed = 8295342;
+//
+//		List<ReactiveDecisionSituation> allSeenSituations = getAllSeenSituations();
+//		Collections.shuffle(allSeenSituations, new Random(shuffleSeed));
+//		allSeenSituations = allSeenSituations.subList(0, numDecisionSituations);
+//
+//		SimpleNichingAlgorithm.clearPopulation(this, allSeenSituations, 0, 1);
+//		allSeenSituations.clear();
+//
+//		// Clear the list so that new situations do not pile on the old ones. Don't know if this is a good idea.
+//		seenSituations.clear();
 	}
 }
