c5bf167bd43cab8327386c0566ea8f4c0c396ec8
Mazhar <mazhar.ansari.ardeh@gmail.com>
2020-05-14 14:14:51 +1200

    Improved WDL functions to also return the name of experiments. Improved documents.

--- python-src/grid/stats.py
+++ python-src/grid/stats.py
@@ -10,7 +10,8 @@ from .core import get_test_fitness
 from .core import sort_algorithms
 from .core import rename_alg
 
-def update_wdl(exp_data, wdltable, rename_map, *, base_line='WithoutKnowledge', num_generations=50):
+def update_wdl(exp_data, wdltable, rename_map, wdltable_exp_names, exp_name, *,
+               base_line='WithoutKnowledge', num_generations=50):
     """
     Computes the Win-Draw-Loss of experiment results given with the experiement data.
     The function does not return any value but updates the input argument 'wdltable'
@@ -32,6 +33,9 @@ def update_wdl(exp_data, wdltable, rename_map, *, base_line='WithoutKnowledge', 
         if not renamed_alg in wdltable:
             # wins, draws, losses, missing 
             wdltable[renamed_alg] = [0, 0, 0, 0]
+        if renamed_alg not in wdltable_exp_names:
+            # wins, draws, losses, missing 
+            wdltable_exp_names[renamed_alg] = [[], [], []]
 
         try:
             mean = statistics.mean(list(exp_data[alg][generation].values()))
@@ -39,10 +43,12 @@ def update_wdl(exp_data, wdltable, rename_map, *, base_line='WithoutKnowledge', 
         except KeyError as e:
             print(alg, e)
             wdltable[renamed_alg][3] += 1
+            wdltable_exp_names[renamed_alg][3].append(exp_name)
             continue
         if len(list(exp_data[base_line][generation].values())) != len(list(exp_data[alg][generation].values())):
             print("Len of ", alg, "(", len(list(exp_data[alg][generation].values())), ") is not 30.")
             wdltable[renamed_alg][3] += 1
+            wdltable_exp_names[renamed_alg][3].append(exp_name)
             # continue
         alg_len = len(list(exp_data[alg][generation].values()))
         pval_wo_wil = stats.wilcoxon(list(exp_data[base_line][generation].values())[:alg_len], list(exp_data[alg][generation].values()))[1]
@@ -50,10 +56,13 @@ def update_wdl(exp_data, wdltable, rename_map, *, base_line='WithoutKnowledge', 
         if pval_wo_wil < 0.05:
             if mean < bmean:
                 wdltable[renamed_alg][0] += 1
+                wdltable_exp_names[renamed_alg][0].append(exp_name)
             else: 
                 wdltable[renamed_alg][2] += 1
+                wdltable_exp_names[renamed_alg][2].append(exp_name)
         else:
             wdltable[renamed_alg][1] += 1
+            wdltable_exp_names[renamed_alg][1].append(exp_name)
 
 def wdl(dirbase, experiments, inclusion_filter, exclusion_filter, rename_map, *, base_line='WithoutKnowledge', num_generations=50, dump_file=Path('./wdl')):
     """
@@ -63,22 +72,26 @@ def wdl(dirbase, experiments, inclusion_filter, exclusion_filter, rename_map, *,
     Usage: wdl(dirbase, experiments, inclusion_filter, exclusion_filter, dump_file=output_folder / 'wdl', rename_map=rename_map)
     """
     wdltable = {}
+    wdltable_exp_name = {}
 
     for exp in experiments: 
         print('WDL: processing', dirbase / exp)
         exp_data = get_test_fitness(dirbase / exp, inclusion_filter, exclusion_filter, num_generations=num_generations)
-        update_wdl(exp_data, wdltable, rename_map, base_line=base_line, num_generations=num_generations)
+        update_wdl(exp_data, wdltable, rename_map, wdltable_exp_name, exp, base_line=base_line, num_generations=num_generations)
 
     with open(str(dump_file) + '.json', 'w') as file: 
         json.dump(wdltable, file, indent=4)
         print('WDL: results saved to:', dump_file)
+
+    with open(str(dump_file) + '-expnames.json', 'w') as file: 
+        json.dump(wdltable_exp_name, file, indent=4)
 
     with open(str(dump_file) + '.csv', 'w', newline="") as csv_file:  
         writer = csv.writer(csv_file)
         for key, value in wdltable.items():
             writer.writerow([key, *value])
 
-    return wdltable
+    return wdltable, wdltable_exp_name
 
 def wdl2(experiment_data, rename_map, *, base_line='WithoutKnowledge', num_generations=50, dump_file=Path('./wdl')):
     """
@@ -89,22 +102,29 @@ def wdl2(experiment_data, rename_map, *, base_line='WithoutKnowledge', num_gener
     Usage: wdl2(experiment_data, dump_file=output_folder / 'wdl', rename_map=rename_map)
     """
     wdltable = {}
+    wdltable_exp_name = {}
 
     for exp in experiment_data: 
         print('WDL2: processing', exp)
         exp_data = experiment_data[exp]
-        update_wdl(exp_data, wdltable, rename_map, base_line=base_line, num_generations=num_generations)
+        update_wdl(exp_data, wdltable, rename_map, wdltable_exp_name, exp, base_line=base_line, num_generations=num_generations)
 
     with open(str(dump_file) + '.json', 'w') as file: 
         json.dump(wdltable, file, indent=4)
         print('WDL2: results saved to:', dump_file)
+
+    with open(str(dump_file) + '-expnames.json', 'w') as file: 
+        json.dump(wdltable_exp_name, file, indent=4)
+        # print('WDL2: results saved to:', dump_file)    
 
     with open(str(dump_file) + '.csv', 'w', newline="") as csv_file:  
         writer = csv.writer(csv_file)
         for key, value in wdltable.items():
             writer.writerow([key, *value])
 
-    return wdltable
+    return wdltable, wdltable_exp_name
+
+def wdl2_exp_name(): pass
 
 def summary(dirbase, experiments, inclusion_filter, exclusion_filter, rename_map,
             *, base_line='WithoutKnowledge', num_generations=50, dump_file=Path('./wdl'), 
@@ -113,7 +133,7 @@ def summary(dirbase, experiments, inclusion_filter, exclusion_filter, rename_map
     test_data_table = {}
     gen = 49
     for exp in experiments: 
-        print('WDL: processing', dirbase / exp)
+        print('Summary: processing', dirbase / exp)
         test_fitness = get_test_fitness(dirbase / exp, inclusion_filter, exclusion_filter, num_generations=num_generations)
         test_data_table[exp] = test_fitness
         if not exp in summary_table:
@@ -203,7 +223,7 @@ def save_summary(summary_table, output_folder, rename_map):
                     + r'\end{table}')
     latex_file.close()
 
-def save_stats(test_fitness, exp_name, output_folder, *, rename_map, gen = 49, round_results = False, baseline_alg = 'WithoutKnowledge'):
+def save_stats(test_fitness, exp_name, output_folder, *, rename_map, gen = 49, round_results = True, baseline_alg = 'WithoutKnowledge'):
     if not Path(output_folder / exp_name).exists(): 
         Path(output_folder / exp_name).mkdir(parents=True)
 
@@ -263,7 +283,9 @@ def save_stats(test_fitness, exp_name, output_folder, *, rename_map, gen = 49, r
 
 
 def improvements(test_fitnesses, rename_map, num_generations = 50, dump_file=Path('./improvements')):
-
+    """
+    Calculates the improvement in training time achieved over baseline algorithm.
+    """
     def find_improvement(fitness, *, base_line='WithoutKnowledge', generations=num_generations):
         retval = {} 
         for alg in fitness:
@@ -327,4 +349,6 @@ def improvements(test_fitnesses, rename_map, num_generations = 50, dump_file=Pat
 
     with open(str(dump_file) + '.json', 'w') as file: 
         json.dump(retval, file, indent=4)
-        print('WDL2: results saved to:', dump_file)
+        print('IMPR: results saved to:', dump_file)
+
+    return retval
