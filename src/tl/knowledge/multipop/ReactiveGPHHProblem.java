2cb537d44fef4953dbb954240022ecc0f4327693
Mazhar Ansari Ardeh <mazhar.ansari.ardeh@gmail.com>
2020-09-01 14:28:02 +1200

    A basic configuration for a multi-population GP.

---
+++ src/tl/knowledge/multipop/ReactiveGPHHProblem.java
@@ -0,0 +1,88 @@
+package tl.knowledge.multipop;
+
+import ec.EvolutionState;
+import ec.Individual;
+import ec.gp.GPIndividual;
+import ec.gp.GPProblem;
+import ec.simple.SimpleProblemForm;
+import ec.util.Parameter;
+import gphhucarp.decisionprocess.DecisionSituation;
+import gphhucarp.decisionprocess.PoolFilter;
+import gphhucarp.decisionprocess.TieBreaker;
+import gphhucarp.decisionprocess.routingpolicy.GPRoutingPolicy;
+import gphhucarp.gp.evaluation.EvaluationModel;
+import tl.gphhucarp.dms.DMSSaver;
+
+import java.util.ArrayList;
+
+public class ReactiveGPHHProblem extends GPProblem implements SimpleProblemForm
+{
+	public static final String P_EVAL_MODEL = "eval-model";
+	public static final String P_POOL_FILTER = "pool-filter";
+	public static final String P_TIE_BREAKER = "tie-breaker";
+
+	protected EvaluationModel[] evaluationModel;
+	protected PoolFilter poolFilter;
+	protected TieBreaker tieBreaker;
+
+	/**
+	 * The number of times that the evaluate function is called. Cloning this object will reset
+	 * this field and therefore, cloning must be disabled with the
+	 * `eval.clone-problem = false`
+	 * line in the parameter file.
+	 */
+	private static int evalCount = 0;
+
+	@Override
+	public void setup(final EvolutionState state, final Parameter base) {
+		super.setup(state, base);
+
+		int nSubPop = state.parameters.getInt(new Parameter("pop.subpops"), null);
+		evaluationModel = new EvaluationModel[nSubPop];
+		for (int subpop = 0; subpop < nSubPop; subpop++)
+		{
+			Parameter p = base.push("subpop").push("" + subpop).push(P_EVAL_MODEL);
+			evaluationModel[subpop] = (EvaluationModel)(
+					state.parameters.getInstanceForParameter(
+							p, null, EvaluationModel.class));
+			evaluationModel[subpop].setup(state, p);
+		}
+
+		Parameter p = base.push(P_POOL_FILTER);
+		poolFilter = (PoolFilter)(
+				state.parameters.getInstanceForParameter(
+						p, null, PoolFilter.class));
+
+		p = base.push(P_TIE_BREAKER);
+		tieBreaker = (TieBreaker)(
+				state.parameters.getInstanceForParameter(
+						p, null, TieBreaker.class));
+	}
+
+	@Override
+	public void evaluate(EvolutionState state,
+						 Individual indi,
+						 int subpopulation,
+						 int threadnum) {
+		GPRoutingPolicy policy =
+				new GPRoutingPolicy(poolFilter, ((GPIndividual)indi).trees[0]);
+
+		// the evaluation model is reactive, so no plan is specified.
+		evaluationModel[subpopulation].evaluate(policy, null, indi.fitness, state);
+		ArrayList<DecisionSituation> seenDecicionSituations = evaluationModel[subpopulation].getSeenDecicionSituations();
+
+		if(state instanceof DMSSaver)
+		{
+			DMSSaver gstate = (DMSSaver) state;
+			boolean saveDMS = gstate.isDMSSavingEnabled();
+			evaluationModel[subpopulation].setSaveDMSEnabled(saveDMS);
+
+			if(saveDMS)
+				gstate.updateSeenSituations(indi, seenDecicionSituations);
+		}
+		this.evaluationModel[subpopulation].resetSeenSituations();
+
+		indi.evaluated = true;
+		evalCount++;
+	}
+}
