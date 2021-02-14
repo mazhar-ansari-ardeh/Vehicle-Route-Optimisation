d243ab0863b536302a84fa456e31ea891176f23a
Mazhar <mazhar.ansari.ardeh@gmail.com>
2021-02-15 11:41:33 +1300

    Added the ability to calculate the entropy of the population. Added the logging of the population and its clusters. This is the version that was used for the GECCO 2021 paper.

--- src/tl/knowledge/multipop/MultiPopEvolutionState.java
+++ src/tl/knowledge/multipop/MultiPopEvolutionState.java
@@ -2,13 +2,17 @@ package tl.knowledge.multipop;
 
 import ec.EvolutionState;
 import ec.Individual;
+import ec.Population;
 import ec.gp.GPIndividual;
 import ec.gp.GPNode;
 import ec.util.Parameter;
 import gphhucarp.decisionprocess.DecisionSituation;
+import gphhucarp.decisionprocess.PoolFilter;
 import gphhucarp.decisionprocess.reactive.ReactiveDecisionSituation;
 import gphhucarp.gp.GPHHEvolutionState;
 import org.apache.commons.math3.stat.descriptive.DescriptiveStatistics;
+import tl.gp.TLGPIndividual;
+import tl.gp.entropy.Entropy;
 import tl.gp.fitness.NormalisedMultiObjectiveFitness;
 
 import java.io.BufferedWriter;
@@ -19,6 +23,7 @@ import java.util.*;
 
 public class MultiPopEvolutionState extends GPHHEvolutionState implements MultiPopDMSSaver
 {
+    private static final String P_MULT_POP_STATE = "multipop-state";
     protected List<TreeMap<Individual, List<DecisionSituation>>> seenSituations = new ArrayList<>();
 
     private final List<ReactiveDecisionSituation> initialSituations = new ArrayList<>();
@@ -29,9 +34,19 @@ public class MultiPopEvolutionState extends GPHHEvolutionState implements MultiP
     public final String P_SAVE_DMS = "save-dms";
     private boolean saveDMS = true;
 
+    /**
+     * The directory to which the population is logged at the end of each generation.
+     */
+    public static final String P_POP_LOG_PATH = "pop-log-path";
+    private String popLogPath;
+
+    protected PoolFilter filter;
+
     @Override
     public void setup(EvolutionState state, Parameter base)
     {
+        if(base == null)
+            base = new Parameter(P_MULT_POP_STATE);
         Parameter p = new Parameter(P_SAVE_DMS);
         saveDMS = parameters.getBoolean(p, null,true);
         super.setup(state, base);
@@ -41,6 +56,18 @@ public class MultiPopEvolutionState extends GPHHEvolutionState implements MultiP
         {
             seenSituations.add(new TreeMap<>());
         }
+
+        p = new Parameter("eval.problem.pool-filter");
+        filter = (PoolFilter) (state.parameters.getInstanceForParameter(p, null, PoolFilter.class));
+        entropy = new Entropy(getInitialSituations().subList(0, 20));
+
+        popLogPath = state.parameters.getString(base.push(P_POP_LOG_PATH), null);
+        if(popLogPath == null)
+        {
+            state.output.fatal("Population log path cannot be null");
+            return;
+        }
+        state.output.warning("Population log path: " + popLogPath);
     }
 
     @Override
@@ -70,7 +97,7 @@ public class MultiPopEvolutionState extends GPHHEvolutionState implements MultiP
     public void writeStatFileTitle(File statFile) {
         try {
             BufferedWriter writer = new BufferedWriter(new FileWriter(statFile));
-            writer.write("Gen,Time,ProgSizeMean,ProgSizeStd,FitMean,FitStd");
+            writer.write("Gen,Time,ProgSizeMean,ProgSizeStd,FitMean,FitStd,Entropy");
             writer.newLine();
             writer.close();
         } catch (IOException e) {
@@ -103,6 +130,8 @@ public class MultiPopEvolutionState extends GPHHEvolutionState implements MultiP
         }
     }
 
+    Entropy entropy;
+
     public void writeToStatFile()
     {
         for(int i = 0; i < this.population.subpops.length; i++)
@@ -116,10 +145,12 @@ public class MultiPopEvolutionState extends GPHHEvolutionState implements MultiP
                         "," + statMap.get(POP_PROG_SIZE).getMean() +
                         "," + statMap.get(POP_PROG_SIZE).getStandardDeviation() +
                         "," + statMap.get(POP_FITNESS).getMean() +
-                        "," + statMap.get(POP_FITNESS).getStandardDeviation()
+                        "," + statMap.get(POP_FITNESS).getStandardDeviation() +
+                        "," + entropy.entropy(population.subpops[i].individuals, 0, filter) + " " + entropy.getnClusters() + " "  + entropy.getnNoise()
                 );
                 writer.newLine();
                 writer.close();
+                logEntropy(entropy, i);
             } catch (IOException e) {
                 e.printStackTrace();
             }
@@ -188,6 +219,54 @@ public class MultiPopEvolutionState extends GPHHEvolutionState implements MultiP
     }
 
     @Override
+    public int evolve() {
+
+        if (generation > 0)
+            output.message("Generation " + generation);
+
+        // EVALUATION
+        statistics.preEvaluationStatistics(this);
+        evaluator.evaluatePopulation(this);
+//	    clear();
+        statistics.postEvaluationStatistics(this);
+
+        finish = util.Timer.getCpuTime();
+        duration = 1.0 * (finish - start) / 1000000000;
+
+        writeToStatFile();
+        logPopulation(population);
+
+        start = util.Timer.getCpuTime();
+
+        // SHOULD WE QUIT?
+        if (evaluator.runComplete(this) && quitOnRunComplete) {
+            output.message("Found Ideal Individual");
+            return R_SUCCESS;
+        }
+
+        // SHOULD WE QUIT?
+        if (generation == numGenerations-1) return R_FAILURE;
+
+        // PRE-BREEDING EXCHANGE
+        if (exchangePopulationPreBreeding()) return R_SUCCESS;
+
+        // BREEDING
+        breed();
+
+        // POST-BREEDING EXCHANGING
+        exchangePopulationPostBreeding();
+
+        // Generate new instances if needed
+        rotateEvalModel();
+
+        // INCREMENT GENERATION AND CHECKPOINT
+        generation++;
+        doCheckpoit();
+
+        return R_NOTDONE;
+    }
+
+    @Override
     public List<ReactiveDecisionSituation> getInitialSituations()
     {
         return initialSituations;
@@ -219,6 +298,31 @@ public class MultiPopEvolutionState extends GPHHEvolutionState implements MultiP
         if (rotateEvalModel) {
             MultiPopReactiveGPHHProblem problem = (MultiPopReactiveGPHHProblem)evaluator.p_problem;
             problem.rotateEvaluationModel();
+        }
+    }
+
+    private void logEntropy(Entropy entropy, int subpop)
+    {
+        int popLogID = setupLogger(this, new File(popLogPath, String.format("pop/Cluster.%d.%d.csv", subpop, generation)).getAbsolutePath());
+        log(this, popLogID, entropy.toString());
+        closeLogger(this, popLogID);
+    }
+
+    protected void logPopulation(Population population)
+    {
+        assert population != null;
+        for(int subpop = 0; subpop < population.subpops.length; subpop++)
+        {
+            Individual[] pop = population.subpops[subpop].individuals;
+            assert pop != null;
+
+            int popLogID = setupLogger(this, new File(popLogPath, String.format("pop/Pop.%d.%d.csv", subpop, generation)).getAbsolutePath());
+            log(this, popLogID, "Origin,Fitness,Tree\n");
+            Arrays.stream(pop).map(i -> (TLGPIndividual) i).sorted(Comparator.comparingDouble(j -> j.fitness.fitness())).forEach(
+                    i -> log(this, popLogID,
+                            i.getOrigin() + "," + i.fitness.fitness() + "," + i.trees[0].child.makeLispTree() + "\n")
+            );
+            closeLogger(this, popLogID);
         }
     }
 }
