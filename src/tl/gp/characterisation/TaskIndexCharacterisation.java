29bbe66433b56f8f4fb71e35e108cae1891a2e2e
Mazhar <mazhar.ansari.ardeh@gmail.com>
2021-06-18 10:39:17 +1200

    Changed the signature of the 'characterise' method to accept RoutingPolicy instead of GPRoutingPolicy. Code cleaning.

--- src/tl/gp/characterisation/TaskIndexCharacterisation.java
+++ src/tl/gp/characterisation/TaskIndexCharacterisation.java
@@ -1,8 +1,8 @@
 package tl.gp.characterisation;
 
 import gphhucarp.core.Arc;
+import gphhucarp.decisionprocess.RoutingPolicy;
 import gphhucarp.decisionprocess.reactive.ReactiveDecisionSituation;
-import gphhucarp.decisionprocess.routingpolicy.GPRoutingPolicy;
 
 import java.util.List;
 
@@ -18,8 +18,6 @@ public class TaskIndexCharacterisation implements RuleCharacterisation<int[]>
 {
 	private final List<ReactiveDecisionSituation> decisionSituations;
 
-//	FileUtils fu = new FileUtils("ti");
-
 	public TaskIndexCharacterisation(final List<ReactiveDecisionSituation> dps)
 	{
 		if(dps == null || dps.isEmpty())
@@ -27,35 +25,17 @@ public class TaskIndexCharacterisation implements RuleCharacterisation<int[]>
 		this.decisionSituations = dps;
 	}
 
-	public int[] characterise(GPRoutingPolicy rule)
+	public int[] characterise(RoutingPolicy rule)
 	{
-//		fu.println(rule.getGPTree().child.makeLispTree());
 		int dsSize = this.decisionSituations.size();
 		final int[] charList = new int[dsSize];
 		for (int i = 0; i < dsSize; i++) {
 			final ReactiveDecisionSituation situation = this.decisionSituations.get(i);
-//			if(rule.getGPTree().child.makeLispTree()
-//					.equalsIgnoreCase("(max (max (* (max CFD (+ FRT CFH)) (- RQ DC)) (* (min (* CFD DC) DEM) (max (min (- SC CTD) (* FUT (* FUT SC))) CFH))) (* (max CFD (max (max (max 0.8416072097232807 CFH) (min DEM DEM)) (max (min (* FUT SC) (+ CFH DEM)) (* CTT1 (* RQ CTD))))) (- RQ DC)))"))
-//			{
-//				final int j = i;
-//				if(j == 6)
-//				{
-//					fu.println("Instance: " + situation.getState().getInstance().toString());
-//				}
-//				fu.println("Pool: ");
-//				situation.getPool().forEach(a -> fu.print(j + ": " + a.toString()));
-//				fu.println("Route: " + situation.getRoute().toString() + ", current node: " + situation.getRoute().currNode());
-//			}
 			final Arc op = rule.next(situation);
 			final int index = situation.getPool().indexOf(op);
-//			if(rule.getGPTree().child.makeLispTree().equalsIgnoreCase("(max (- RQ CFH) (* (max CFD (max (max (max 0.8416072097232807 (- SC CTD)) CFD) (max (min (- RQ CFH) (* CFD DC)) (* CTT1 (* RQ CTD))))) (min (max (max (+ CFH DEM) (- SC CTD)) (* CTT1 RQ)) (* (max CFD (- SC CTD)) (- RQ DC)))))"))
-//			{
-//				fu.println("Selected: " + index + ", " + (op != null ? op.toString() + "pr: " + op.getPriority() + "\n" : "null"));
-//			}
+
 			charList[i] = index;
 		}
-//		fu.println(Arrays.toString(charList));
-//		fu.println();
 		return charList;
 	}
 }
