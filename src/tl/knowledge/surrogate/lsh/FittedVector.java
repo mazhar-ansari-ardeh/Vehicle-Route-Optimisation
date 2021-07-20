11df354d70b6f599948b76c4a89cfc7a00694772
Mazhar <mazhar.ansari.ardeh@gmail.com>
2021-07-20 16:50:27 +1200

    Added a field for the generation of the individual. Updated the document.

--- src/tl/knowledge/surrogate/lsh/FittedVector.java
+++ src/tl/knowledge/surrogate/lsh/FittedVector.java
@@ -4,6 +4,10 @@ import tl.knowledge.sst.lsh.Vector;
 
 import java.util.List;
 
+/**
+ * An implementation of the {@link Vector} class that stores the fitness of individual it is representing, as well as
+ * the generation it is from.
+ */
 public class FittedVector extends Vector
 {
     private double fitness = Double.POSITIVE_INFINITY;
@@ -11,6 +15,13 @@ public class FittedVector extends Vector
     public double getFitness()
     {
         return fitness;
+    }
+
+    private int generation;
+
+    public int getGeneration()
+    {
+        return generation;
     }
 
     public FittedVector(int dimensions)
@@ -22,23 +33,29 @@ public class FittedVector extends Vector
     {
         super(other);
         if(other instanceof FittedVector)
+        {
             this.fitness = ((FittedVector) other).fitness;
+            this.generation = ((FittedVector) other).generation;
+        }
     }
 
-    public FittedVector(String key, double[] values, double fitness)
+    public FittedVector(String key, double[] values, double fitness, int generation)
     {
         super(key, values);
         this.fitness = fitness;
+        this.generation = generation;
     }
 
-    public FittedVector(int[] values, double fitness) {
+    public FittedVector(int[] values, double fitness, int generation) {
         super(values);
         this.fitness = fitness;
+        this.generation = generation;
     }
 
-    public FittedVector(List<? extends Number> values, double fitness)
+    public FittedVector(List<? extends Number> values, double fitness, int generation)
     {
         super(values);
         this.fitness = fitness;
+        this.generation = generation;
     }
 }
