463080237904bf237a3af6ff63cde93f6276e04c
Mazhar <mazhar.ansari.ardeh@gmail.com>
2021-07-20 13:44:57 +1200

    Rewrote a simpler version of the class that does not require an extra but redundant field.

--- src/tl/knowledge/sst/SSTIndividual.java
+++ src/tl/knowledge/sst/SSTIndividual.java
@@ -4,26 +4,8 @@ import tl.gp.TLGPIndividual;
 
 public class SSTIndividual extends TLGPIndividual
 {
-	private IndividualOrigin origin;
-
-	@Override
-	public String getOrigin()
-	{
-		if(origin == null)
-			return null;
-		return origin.toString();
-	}
-
 	public void setOrigin(IndividualOrigin origin)
 	{
-		this.origin = origin;
-	}
-
-	public SSTIndividual lightClone()
-	{
-		SSTIndividual ind = (SSTIndividual) super.lightClone();
-		ind.origin = origin;
-
-		return ind;
+		this.origin = origin.toString();
 	}
 }
